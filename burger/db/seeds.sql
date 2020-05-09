63fcc209c790486a570dbabcc43e20538301c288
vsaleem <vic.saleem@gmail.com>
2020-05-08 22:43:37 -0400

    Added seed data.

---
+++ burger/db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers ( ID, burger_name, devoured)
+VALUES (1, 'CHEESEBURGER', true);
+
+INSERT INTO burgers ( ID, burger_name, devoured)
+VALUES (1, 'GOAT BURGER', FALSE);
+
+INSERT INTO burgers ( ID, burger_name, devoured)
+VALUES (8, 'HAM N CHEESE', true);
