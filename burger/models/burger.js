8e2efe9c7e36ae0f89c448860fa09d42b4a723ba
vsaleem <vic.saleem@gmail.com>
2020-05-16 22:08:58 -0400

    Added methods that operates mysql controllers; required orm file.

--- burger/models/burger.js
+++ burger/models/burger.js
@@ -2,5 +2,23 @@ const orm = require('../config/orm');
 
 
 // CREATE CODE THAT CALLS THE ORM FUNCTIONS USING BURGER SPECIFIC INPUT FOR THE ORM.
+// VARIABLE THAT CONTAINS METHODS TO OPERATE MYSQL CONTROLLERS - OBJECT RELATIONAL MAPPING
+var burger = {
+    selectAll: function(cb){
+        orm.selectAll("burgers", function(res){
+            cb(res);
+        });
+    },
+    insertOne: function(cols, vals, cb){
+        orm.insertOne("burgers", cols, vals, function(res){
+            cb(res);
+        });
+    },
+    updateOne: function(vals, condition, cb){
+        orm.updateOne("burgers", vals, condition, function(res){
+            cb(res);
+        });
+    }
+};
 
-module.exports = orm
+module.exports = burger;
