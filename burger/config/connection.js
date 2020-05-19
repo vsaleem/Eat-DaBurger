76d2510f86ba2c64a87a3ab82418cc647f25fec2
vsaleem <vic.saleem@gmail.com>
2020-05-19 08:35:34 -0400

    Updated connection; added JAWSDB_URL env var to connect to JawsDB db.

--- burger/config/connection.js
+++ burger/config/connection.js
@@ -1,13 +1,18 @@
 const mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: '',
-    password: "",
-    database: 'burgers_db'
+var connection;
 
-});
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: '',
+        password: "",
+        database: 'burgers_db'
+    });
+};
 
 connection.connect(function(err){
     if (err) throw err;
