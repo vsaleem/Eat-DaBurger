111a8b687d594a3ae06a7df363d22d5cee5db4a9
vsaleem <vic.saleem@gmail.com>
2020-05-19 13:56:58 -0400

    Updated connection; added JAWSDB_URL env var to connect to JawsDB db.

--- burger/db/schema.sql
+++ burger/db/schema.sql
@@ -6,6 +6,6 @@ CREATE TABLE burgers (
     id INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(30),
     devoured BOOLEAN,
-    createdAt TIMESTAMP NOT NULL,
+    createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(id)
 );
