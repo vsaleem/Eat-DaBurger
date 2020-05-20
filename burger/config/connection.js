1ddd73631ca9cf060a23a1833d4a4a8eeed6754a
vsaleem <vic.saleem@gmail.com>
2020-05-19 22:28:35 -0400

    Modified connection file to test Jawsdb connection.

--- burger/config/connection.js
+++ burger/config/connection.js
@@ -1,13 +1,19 @@
 const mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: '',
-    password: "",
-    database: 'burgers_db'
+var connection;
 
-});
+if(process.env.JAWSDB_URL){
+    connenction = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: 'vsaleem',
+        password: "vsaleem11!!5",
+        database: 'burgers_db'
+    });
+};
+
 
 connection.connect(function(err){
     if (err) throw err;
